2ae3ffed98a6ff20bc18e680bc3a11327ec8b52d
Gyunny <wjdrbs966@naver.com>
2021-01-03 00:03:17 +0900

    Java 8에 추가된 것들

--- Java_God/src/Test/Test.java
+++ Java_God/src/Test/Test.java
@@ -1,7 +1,13 @@
 package Test;
 
-public class Test {
+import java.util.Arrays;
 
+public class Test {
     public static void main(String[] args) {
+        int[] list = {5, 4, 2, 7, 10, 9, 1, 6, 3, 8};
+
+        Arrays.parallelSort(list);
+        System.out.println(Arrays.toString(list));  // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     }
 }
+
