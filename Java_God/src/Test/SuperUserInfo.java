348707e5f6c344b6d1d47a3a778e3ec5c792062e
Gyunny <wjdrbs966@naver.com>
2021-01-04 15:22:46 +0900

    직렬화 내용 수정

---
+++ Java_God/src/Test/SuperUserInfo.java
@@ -0,0 +1,8 @@
+package Test;
+
+import java.io.Serializable;
+
+public class SuperUserInfo implements Serializable {
+    String name;
+    String password;
+}
---
+++ Java_God/src/Test/UserInfo.java
@@ -0,0 +1,7 @@
+package Test;
+
+public class UserInfo extends SuperUserInfo {
+    int age;
+
+    Object object = new String("abc");
+}
