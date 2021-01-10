e077048b624c4ada8ab8bfd7380e63e012985789
Gyunny <wjdrbs966@naver.com>
2021-01-10 20:22:03 +0900

    BigDecimal이란?

--- Java_God/src/Test/Test.java
+++ Java_God/src/Test/Test.java
@@ -1,13 +1,15 @@
 package Test;
 
-import java.util.Arrays;
-import java.util.List;
+import java.math.BigDecimal;
 
 public class Test {
     public static void main(String[] args) {
-        List<Integer> intList = Arrays.asList(1, 2, 3, 4, 5, 6);
-
-        intList.stream().map(x -> x * 3).forEach(System.out::println);
+        BigDecimal value = new BigDecimal("1.0");
+        BigDecimal addValue = new BigDecimal(0.1);
+        for (int i = 0; i < 10; ++i) {
+            value = value.add(addValue);
+            System.out.println(value.toString());
+        }
     }
 }
 
